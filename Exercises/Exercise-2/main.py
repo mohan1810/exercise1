6e31c5f9913433da5292e02aeccad7e1e0655be4
Mohan Sharma <rockinmax@MAINGULTBY0186.local>
2022-05-04 00:56:06 +0530

    change

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,12 +1,22 @@
 import requests
-import pandas
+import pandas as pd
+import re
 
 
-
-def main():
-    # your code here
-    pass
+def main(url):
+    data  = requests.get(url)
+    html_data = data.content.decode('utf-8')
+    x = re.findall('href.*2022-02-07 14:03',html_data)[0]
+    a = x.split('"')[1]
+    new_url = url + a
+    csv_data = requests.get(new_url).content.decode('utf-8')
+    with open('csv_data.csv','w') as file:
+        file.write(csv_data)
+    df = pd.read_csv('csv_data.csv')
+    m = df['HourlyDryBulbTemperature'].max()
+    print(m)
 
 
 if __name__ == '__main__':
-    main()
+    url = 'https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/'
+    main(url)
