1cbc5ef8baab1de93e5d0a6d86a013c2721eb261
Mohan Sharma <rockinmax@MAINGULTBY0186.local>
2022-05-16 15:37:14 +0530

    ex-4

--- Exercises/Exercise-3/main.py
+++ Exercises/Exercise-3/main.py
@@ -1,9 +1,14 @@
 import boto3
-
+import requests
 
 def main():
-    # your code here
-    pass
+    BUCKET_NAME = 'commoncrawl'
+    KEY = 'crawl-data/CC-MAIN-2022-05/wet.paths.gz'
+    s3 = boto3.resource('s3')
+   
+    s3.Bucket(BUCKET_NAME).download_file(KEY, 's3://commoncrawl/crawl-data/CC-MAIN-2022-05')
+ 
+
 
 
 if __name__ == '__main__':
--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,10 +1,27 @@
+from logging import exception
 import boto3
-
-
+import os
+import json
+import logging
+files_list = []
+import pandas as pd
+if not os.path.isdir('downloads'):
+    os.makedirs('downloads')
 def main():
-    # your code here
-    pass
+    for roots,files in os.walk('data',topdown=True):
+        for file in files:
+            if '.json' in file:
+                files_list.append(roots+'/'+file)
+    # print(files_list)          
+    for a,i in enumerate(files_list):
+            with open(i) as file:
+                data = json.load(file)
+            # print(data)
+            x = pd.json_normalize(data)
+            x.to_csv('downloads/file{0}.csv'.format(a+1))
+           
 
 
 if __name__ == '__main__':
     main()
+    
